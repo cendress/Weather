18c5a03de440e3b68b76b119af14b9fb10f065cd
cendress <95492688+cendress@users.noreply.github.com>
2023-09-18 16:22:04 -0400

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Christopher Endress on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
